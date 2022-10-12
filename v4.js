d637f7eb77d0bc37ba469ca8b88081a04c452ee6
Ferreirabob <nunesrobertferreira@gmail.com>
2022-10-11 23:20:39 -0300

    commit_all

--- v4.js
+++ v4.js
@@ -23,23 +23,28 @@ console.log("*** Choice one option ***");
 console.table(options);
 console.log("**************************");
 
-let selectOption = answer("Select one Option by Number: ") 
+let selectOption = answer("Select one Option by Number: ");
+
 switch (selectOption) {
-    case ("Create", "0"):
-      console.log("Your option selected is Create");
-      break;
-    case ("Read:All", "1"):
-      console.log("Your option selected is Read:All");
-      break;
-    case ("Read:One", "2"):
-      console.log("Your option selected is Read:One");
-      break;
-    case ("Update", "3"):
-      console.log("Your option selected is Update");
-      break;
-    case ("Delete", "4"):
-      console.log("Your option selected is Delete");
-      break;
-    default:
-      console.log("Your option selected don't exist.");
-  }
+  case "0":
+    console.log("Your option selected is Create");
+    break;
+  case "1":
+    console.log("Your option selected is Read:All");
+    break;
+  case "2":
+    console.log("Your option selected is Read:One");
+    break;
+  case "3":
+    console.log("Your option selected is Update");
+    break;
+  case "4":
+    console.log("Your option selected is Delete");
+    break;
+  default:
+    console.log("Your option selected don't exist.");
+}
+
+while (selectOption !== "z") {
+  return selectOption;
+}
